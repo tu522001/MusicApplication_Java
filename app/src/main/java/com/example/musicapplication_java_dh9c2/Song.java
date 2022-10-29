a6070d19c01e27a571515322dabb935646909863
Xuan_Truong <bichkhoai2001@gmail.com>
2022-10-29 13:13:57 +0700

    L2
    Signed-off-by: Xuan_Truong <bichkhoai2001@gmail.com>

---
+++ app/src/main/java/com/example/musicapplication_java_dh9c2/Song.java
@@ -0,0 +1,59 @@
+package com.example.musicapplication_java_dh9c2;
+
+import java.io.Serializable;
+
+public class Song implements Serializable {
+    int Id;
+    String title;
+    String singer;
+    int file;
+    int image;
+
+    public Song(int id, String title, String singer, int file, int image) {
+        Id = id;
+        this.title = title;
+        this.singer = singer;
+        this.file = file;
+        this.image = image;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public int getFile() {
+        return file;
+    }
+
+    public void setFile(int file) {
+        this.file = file;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
---
+++ app/src/main/java/com/example/musicapplication_java_dh9c2/data/Database.java
@@ -0,0 +1,37 @@
+package com.example.musicapplication_java_dh9c2.data;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper {
+    public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    // Truy vấn không trả kết quả : create , insert , update ,delete...
+    public void QueryData(String sql){
+        // ghi vào dữ liệu vào getWritableDatabase();
+        SQLiteDatabase database = getWritableDatabase();
+        database.execSQL(sql);
+    }
+
+    // Truy vấn có trả về : Select
+    public Cursor GetData(String sql){
+        SQLiteDatabase database = getReadableDatabase();
+        return database.rawQuery(sql,null);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
